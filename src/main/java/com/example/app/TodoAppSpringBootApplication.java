cb483c1617d274f54f6b8e4658521f7cbbc49c32
FujiokaSei <f.sei3123@gmail.com>
2022-02-09 15:47:39 +0900

    First commit

---
+++ src/main/java/com/example/app/TodoAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppSpringBootApplication.class, args);
+	}
+
+}
